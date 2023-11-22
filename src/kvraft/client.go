d322611084fa88d9f4c015b224390d6d0ebdfc77
LT0X <2756771826@qq.com>
2023-11-22 19:38:21 +0800

    此次完成3A的所有测试

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,12 +1,26 @@
 package kvraft
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"sync/atomic"
+	"time"
 
+	"6.5840/labrpc"
+)
+
+//表示分配给op的唯一Id
+var opId int32
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
+
+	channelPool *ChannelPool //客户端使用的管道池
+
+	leaderIndex int //表示Service leader的索引缓存
+
+	// serverLastOpId int
 	// You will have to modify this struct.
 }
 
@@ -20,8 +34,41 @@ func nrand() int64 {
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
+	ck.leaderIndex = int(nrand()) % len(servers)
+	ck.channelPool = NewChannelPool(4)
+
+	//等待raft初步启动
+	time.Sleep(300 * time.Millisecond)
 	// You'll have to add code here.
 	return ck
+}
+
+func (ck *Clerk) monitorRPCRunTime(channel chan bool,
+	gReply *GetReply, pReply *PutAppendReply) {
+
+	time.Sleep(time.Millisecond * 800)
+
+	if gReply != nil {
+
+		if !gReply.Done {
+
+			//当前缓存server不能正常处理
+			gReply.NotLeader = true
+			channel <- true
+		}
+		return
+	}
+
+	if pReply != nil {
+
+		if !pReply.Done {
+			//当前缓存server不能正常处理
+			pReply.NotLeader = true
+			channel <- true
+		}
+		return
+	}
+
 }
 
 // fetch the current value for a key.
@@ -35,9 +82,97 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
-	return ""
+
+	args := GetArgs{
+
+		Key: key,
+		Id:  int(atomic.AddInt32(&opId, 1)),
+	}
+	reply := GetReply{
+		NotLeader: false,
+	}
+	fmt.Printf("客户端开始发送  opid %v op %v \n", args.Id, args)
+
+	// channel := ck.channelPool.GetChannel(int32(args.Id))
+
+	// go ck.monitorRPCRunTime(channel, &reply, nil)
+	// start := time.Now()
+	for !reply.Done {
+
+		if reply.NotLeader {
+			ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+
+		}
+
+		// ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+		// fmt.Printf("发送 kv 的缓存 %v\n", ck.leaderIndex)
+		ok := ck.sendGetRPC(ck.leaderIndex, &args, &reply)
+
+		if !ok {
+			fmt.Printf("客户端发起RPC失败 opid %v,reply %v,cklindex %v\n",
+				args.Id, reply, ck.leaderIndex)
+
+			reply.NotLeader = true
+			// if ck.serverLastOpId >= args.Id {
+			// 	reply.Done = true
+			// 	fmt.Printf("客户端退出了 opid %v\n", args.Id)
+			// }
+		}
+
+	}
+
+	// for !reply.Done {
+
+	// 	// if reply.NotLeader {
+	// 	// 	ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+	// 	// }
+
+	// 	go ck.sendGetRPCRunTime(&args, &reply, channel)
+
+	// 	select {
+	// 	case v, _ := <-channel:
+	// 		if v {
+	// 			continue
+	// 		}
+	// 	}
+
+	// }
+	// dur := time.Since(start)
+	// fmt.Printf("!!!!!! Get花费的时间为 %v\n", dur)
+
+	fmt.Printf("opid %v get 完成了 \n", args.Id)
+	return reply.Value
+}
+
+func (ck *Clerk) sendGetRPC(index int, args *GetArgs, reply *GetReply) bool {
+
+	return ck.servers[index].Call("KVServer.Get", args, reply)
+}
+
+func (ck *Clerk) sendGetRPCRunTime(
+	args *GetArgs, reply *GetReply, channel chan bool) {
+
+	ok := false
+	for !ok {
+
+		if reply.NotLeader {
+			ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+		}
+
+		ok = ck.sendGetRPC(ck.leaderIndex, args, reply)
+		if !ok {
+			fmt.Printf("客户端发起RPC失败 opid %v,reply %v\n", args.Id, reply)
+			reply.NotLeader = true
+			// if ck.serverLastOpId >= args.Id {
+			// 	reply.Done = true
+			// 	fmt.Printf("客户端退出了 opid %v\n", args.Id)
+			// }
+		}
+	}
+	fmt.Printf("opid %v 结束了 \n", args.Id)
+	channel <- ok
+
 }
 
 // shared by Put and Append.
@@ -50,6 +185,109 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+
+	args := PutAppendArgs{
+		Key:   key,
+		Value: value,
+		Op:    op,
+		Id:    int(atomic.AddInt32(&opId, 1)),
+	}
+	reply := PutAppendReply{
+		NotLeader: false,
+	}
+	fmt.Printf("客户端开始发送  opid %v args %v\n", args.Id, args)
+
+	// channel := ck.channelPool.GetChannel(int32(args.Id))
+
+	// go ck.monitorRPCRunTime(channel, nil, &reply)
+
+	for !reply.Done {
+
+		if reply.NotLeader {
+			ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+
+		}
+
+		// ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+		// fmt.Printf("发送 kv 的缓存 %v\n", ck.leaderIndex)
+		ok := ck.sendPutAppendRPC(ck.leaderIndex, &args, &reply)
+
+		if !ok {
+			fmt.Printf("客户端发起RPC失败 opid %v,reply %v,cklindex %v\n",
+				args.Id, reply, ck.leaderIndex)
+
+			reply.NotLeader = true
+			// if ck.serverLastOpId >= args.Id {
+			// 	reply.Done = true
+			// 	fmt.Printf("客户端退出了 opid %v\n", args.Id)
+			// }
+		}
+
+	}
+
+	fmt.Printf("opid %v put 完成了 \n", args.Id)
+
+	// // start := time.Now()
+	// for !reply.Done {
+
+	// 	// if !reply.IsLeader {
+	// 	// 	ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+	// 	// }
+
+	// 	go ck.sendPutAppendRPCRunTime(&args, &reply, channel)
+
+	// 	select {
+	// 	case v, _ := <-channel:
+	// 		if v {
+	// 			continue
+	// 		}
+	// 	}
+
+	// }
+
+	// dur := time.Since(start)
+	// fmt.Printf("!!!!!! put花费的时间为 %v\n", dur)
+	// fmt.Printf("||||||||||||||||")
+
+}
+
+func (ck *Clerk) sendPutAppendRPC(index int, args *PutAppendArgs, reply *PutAppendReply) bool {
+
+	// fmt.Printf("准备发送索引是 %v\n", index)
+	return ck.servers[index].Call("KVServer.PutAppend", args, reply)
+}
+
+func (ck *Clerk) sendPutAppendRPCRunTime(
+	args *PutAppendArgs, reply *PutAppendReply, channel chan bool) {
+
+	ok := false
+	for !ok {
+
+		if reply.NotLeader {
+			ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+
+		}
+
+		// ck.leaderIndex = (ck.leaderIndex + 1) % len(ck.servers)
+		// fmt.Printf("发送 kv 的缓存 %v\n", ck.leaderIndex)
+		ok = ck.sendPutAppendRPC(ck.leaderIndex, args, reply)
+		if !ok {
+			fmt.Printf("客户端发起RPC失败 opid %v,reply %v,cklindex %v\n",
+				args.Id, reply, ck.leaderIndex)
+
+			reply.NotLeader = true
+
+			// if ck.serverLastOpId >= args.Id {
+			// 	reply.Done = true
+			// 	fmt.Printf("客户端退出了 opid %v\n", args.Id)
+			// }
+		}
+	}
+
+	// fmt.Printf("opid 客户端结束 %v reply %v\n", args.Id, reply)
+	fmt.Printf("opid %v 结束了 \n", args.Id)
+	channel <- true
+
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -1,15 +1,28 @@
 package kvraft
 
 import (
-	"6.5840/labgob"
-	"6.5840/labrpc"
-	"6.5840/raft"
+	"fmt"
 	"log"
 	"sync"
 	"sync/atomic"
+	"time"
+
+	"6.5840/labgob"
+	"6.5840/labrpc"
+	"6.5840/raft"
 )
 
 const Debug = false
+
+// var opIdMap map[int]bool //用于检查opid 是否已经被执行。
+
+// var channelPool ChannelPool
+
+// func init() {
+// 	fmt.Printf("servicev 进行初始化操作\n")
+
+// 	opIdMap = make(map[int]bool, 300)
+// }
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
@@ -18,37 +31,373 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
+	Optype string
+	Key    string
+	Value  string
+	Id     int
+
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
 }
 
 type KVServer struct {
-	mu      sync.Mutex
-	me      int
-	rf      *raft.Raft
-	applyCh chan raft.ApplyMsg
-	dead    int32 // set by Kill()
+	mu          sync.Mutex
+	me          int
+	rf          *raft.Raft
+	applyCh     chan raft.ApplyMsg
+	kvMap       map[string]string //本地service键值对
+	channelPool ChannelPool       //管道池
+	opIdMap     map[int]bool
+	clientMap   map[int]bool //表示客户端是否还和Server链接
+
+	lastOpId int   //Cleark 最后的opId
+	dead     int32 // set by Kill()
+
+	rwLock sync.RWMutex
 
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
 }
 
+func (kv *KVServer) getMapValue(k string) (string, bool) {
+	kv.rwLock.RLock()
+	defer kv.rwLock.RUnlock()
+	v, ok := kv.kvMap[k]
+
+	return v, ok
+}
+
+func (kv *KVServer) putMapValue(k string, v string) {
+	kv.rwLock.Lock()
+
+	defer kv.rwLock.Unlock()
+
+	kv.kvMap[k] = v
+
+}
+
+func (kv *KVServer) appendMapValue(k string, v string) {
+	kv.rwLock.Lock()
+	defer kv.rwLock.Unlock()
+
+	kv.kvMap[k] = kv.kvMap[k] + v
+
+}
+
+func (kv *KVServer) getSyncMap(key int, maps map[int]bool) (bool, bool) {
+
+	kv.rwLock.RLock()
+	defer kv.rwLock.RUnlock()
+
+	v, ok := maps[key]
+	return v, ok
+
+}
+
+func (kv *KVServer) setSyncMap(key int, value bool, maps map[int]bool) {
+	kv.rwLock.Lock()
+	defer kv.rwLock.Unlock()
+
+	maps[key] = value
+
+}
+
+func (kv *KVServer) applyChRunTime() {
+
+	for true {
+		select {
+
+		case msg, _ := <-kv.applyCh:
+
+			if op, ok := msg.Command.(Op); ok {
+				fmt.Printf("测试《《《 : opid %v me %v op %v \n", op.Id, kv.me, op)
+
+				if _, ok := kv.rf.GetState(); ok {
+					c := kv.channelPool.GetChannel(int32(op.Id))
+					_, ok := kv.rf.GetState()
+					fmt.Printf("向管道发送结束请求 opID %v kvindex %v isLeader %v\n", op.Id, kv.me, ok)
+
+					// kv.rwLock.RLock()
+
+					// // v, ok := kv.clientMap[op.Id]
+
+					// kv.rwLock.RUnlock()
+
+					v, ok := kv.getSyncMap(op.Id, kv.clientMap)
+
+					if v && ok {
+						c <- op.Id
+					}
+
+					fmt.Printf("这个才是测试 me %v\n", kv.me)
+				}
+
+				// v, _ := kv.opIdMap[op.Id]
+
+				if v, _ := kv.getSyncMap(op.Id, kv.opIdMap); v {
+					fmt.Printf("结束了 *********** 错误 error opid %v me %v\n",
+						op.Id, kv.me)
+					continue
+				}
+
+				kv.executeOp(op)
+				kv.setSyncMap(op.Id, true, kv.opIdMap)
+				// kv.opIdMap[op.Id] = true
+
+				fmt.Printf("结束了 ***********opid %v me %v\n", op.Id, kv.me)
+
+			}
+
+		}
+	}
+}
+
+func (kv *KVServer) executeOp(op Op) {
+
+	if op.Optype == "Put" {
+
+		kv.putMapValue(op.Key, op.Value)
+
+	} else if op.Optype == "Append" {
+
+		if _, ok := kv.getMapValue(op.Key); !ok {
+
+			kv.putMapValue(op.Key, op.Value)
+
+			return
+		}
+		// kv.kvMap[op.Key] = kv.kvMap[op.Key] + op.Value
+		kv.appendMapValue(op.Key, op.Value)
+
+	}
+}
+
+func (kv *KVServer) monitorChannel(channel chan bool, opid int) {
+
+	//对每次处理客户端请求进行监视
+	//检查管道是否因为异常长时间不释放
+	//如果800ms还未处理完成，则认为异常，强制结束请求
+
+	time.Sleep(time.Millisecond * 3000)
+
+	// v, ok := kv.opIdMap[opid]
+	v, ok := kv.getSyncMap(opid, kv.opIdMap)
+	if !ok || !v {
+		fmt.Printf("监管已经去除 opid %v ok %v,v %v\n", opid, ok, v)
+		channel <- false
+	}
+
+	// fmt.Printf("opid %v me %v 开始判断管道超时 v %v ok %v \n",
+	// 	opid, kv.me, v, ok)
+
+}
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+
+	op := Op{
+		Optype: "Get",
+		Key:    args.Key,
+		Id:     args.Id,
+	}
+
+	_, isLeader := kv.rf.GetState()
+
+	if v, ok := kv.getSyncMap(args.Id, kv.opIdMap); isLeader && v && ok {
+		//已经存在，提前返回
+		reply.Done = true
+		v, IsExit := kv.getMapValue(args.Key)
+		reply.ServerLastOpId = kv.lastOpId
+		fmt.Printf("已经处理，提前释放 opid %v \n", args.Id)
+		if !IsExit {
+			reply.Value = ""
+			return
+		}
+		reply.Value = v
+		return
+	}
+
+	_, _, isLeader = kv.rf.Start(op)
+
+	// fmt.Printf("raft index %v me %v    ", x, kv.me)
+
+	if !isLeader {
+		if !isLeader {
+			//不是leader节点，需要重新返回重试
+			reply.NotLeader = true
+
+			return
+		}
+	}
+
+	//表示客户端正在于服务器链接
+
+	// kv.rwLock.Lock()
+	// kv.clientMap[args.Id] = true
+	// kv.rwLock.Unlock()
+
+	kv.setSyncMap(args.Id, true, kv.clientMap)
+
+	defer func() {
+		// kv.rwLock.Lock()
+		// kv.clientMap[args.Id] = false
+		// kv.rwLock.Unlock()
+		kv.setSyncMap(args.Id, false, kv.clientMap)
+	}()
+
+	fmt.Printf("管道开始监听 opid %v me %v\n", args.Id, kv.me)
+	c := kv.channelPool.GetChannel(int32(args.Id))
+	// start := time.Now()
+
+	// go kv.monitorChannel(c, args.Id)
+
+	for true {
+
+		select {
+		case isok, _ := <-c:
+
+			if isok == args.Id {
+
+				// v, IsExit := kv.kvMap[args.Key]
+				v, IsExit := kv.getMapValue(args.Key)
+				reply.Done = true
+				reply.ServerLastOpId = kv.lastOpId
+				// fmt.Printf("管道准备释放 opid %v \n", args.Id)
+				if !IsExit {
+					reply.Value = ""
+
+					return
+				}
+
+				reply.Value = v
+				// dur := time.Since(start)
+				// fmt.Printf("!!!!!! Get花费的时间为 %v\n", dur)
+				return
+			} else {
+				continue
+			}
+			// else {
+			// 	fmt.Printf("监管函数 强制下线 opid %v me %v\n", args.Id, kv.me)
+			// 	reply.NotLeader = true
+			// 	return
+			// }
+
+		case <-time.After(3 * time.Second):
+			// 2s服务器未能处理，表示出现异常，返回false
+			fmt.Printf("监管函数 强制下线 opid %v me %v\n", args.Id, kv.me)
+			reply.NotLeader = true
+			return
+		}
+
+	}
+
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	// fmt.Printf("|||||||||||||||||||||||")
+
+	if v, ok := kv.getSyncMap(args.Id, kv.opIdMap); v && ok {
+		reply.Done = true
+		return
+	}
+	op := Op{
+		Optype: args.Op,
+		Key:    args.Key,
+		Value:  args.Value,
+		Id:     args.Id,
+	}
+
+	_, _, isLeader := kv.rf.Start(op)
+
+	// fmt.Printf("raft index %v me %v    ", x, kv.me)
+
+	// fmt.Printf("<<<<< i %v term %v, isLeader %v\n", i, i2, isLeader)
+
+	if !isLeader {
+		//不是leader节点，需要重新返回重试
+		// fmt.Printf("不是leader，开始 返回 %v\n", kv.me)
+		reply.NotLeader = true
+
+		return
+	}
+
+	//表示客户端正在于服务器链接
+	// kv.rwLock.Lock()
+	// kv.clientMap[args.Id] = true
+	// kv.rwLock.Unlock()
+
+	// defer func() {
+	// 	kv.rwLock.Lock()
+	// 	kv.clientMap[args.Id] = false
+	// 	kv.rwLock.Unlock()
+	// }()
+
+	kv.setSyncMap(args.Id, true, kv.clientMap)
+
+	defer func() {
+		// kv.rwLock.Lock()
+		// kv.clientMap[args.Id] = false
+		// kv.rwLock.Unlock()
+		kv.setSyncMap(args.Id, false, kv.clientMap)
+	}()
+
+	// fmt.Printf("管道开始监听 opid %v me %v \n", args.Id, kv.me)
+	c := kv.channelPool.GetChannel(int32(args.Id))
+	// start := time.Now()
+
+	// go kv.monitorChannel(c, args.Id)
+
+	for true {
+
+		select {
+
+		case isok, _ := <-c:
+			// fmt.Printf("！！！！！！！！ isok me %v %v\n", isok, kv.me)
+
+			if isok == args.Id {
+				fmt.Printf("管道准备释放 opid %v me %v\n", args.Id, kv.me)
+				reply.Done = true
+				reply.ServerLastOpId = kv.lastOpId
+				// dur := time.Since(start)
+				// fmt.Printf("!!!!!! put花费的时间为 %v opid %v\n", dur, args.Id)
+				return
+			} else {
+				continue
+			}
+			// else {
+
+			// 	fmt.Printf("监管函数 强制下线 opid %v me %v\n", args.Id, kv.me)
+
+			// 	return
+			// }
+		case <-time.After(3 * time.Second):
+			fmt.Printf("监管函数 强制下线 opid %v me %v\n", args.Id, kv.me)
+			reply.NotLeader = true
+			return
+		}
+
+	}
+
+	// kv.executeOp(*op)
+
+	// select {
+
+	// case msg := <-kv.applyCh:
+	// 	if op, ok := msg.Command.(Op); ok {
+
+	// 		fmt.Printf("测试《《《 : opid %v msg id %v \n", args.id, op.id)
+	// 	}
+	// }
+
 }
 
-// the tester calls Kill() when a KVServer instance won't
+// thetester calls Kill() when a KVServer instance won't
 // be needed again. for your convenience, we supply
-// code to set rf.dead (without needing a lock),
+// code to set rf.dead ( without needing a lock),
 // and a killed() method to test rf.dead in
 // long-running loops. you can also add your own
 // code to Kill(). you're not required to do anything
@@ -85,12 +434,19 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv := new(KVServer)
 	kv.me = me
 	kv.maxraftstate = maxraftstate
+	kv.kvMap = make(map[string]string)
+	kv.channelPool = *NewChannelPool(80)
+
+	kv.opIdMap = make(map[int]bool, 300)
+
+	kv.clientMap = make(map[int]bool, 300)
 
 	// You may need initialization code here.
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
+	go kv.applyChRunTime()
 	// You may need initialization code here.
 
 	return kv
